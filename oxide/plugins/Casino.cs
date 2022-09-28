6ffb56226f4ea3ae8bfd6f09f04238407fe50a35
shadowfearzxc <kurilik.a.12@mail.ru>
2022-09-28 16:19:12 +0300

    update v1.0.2

--- oxide/plugins/Casino.cs
+++ oxide/plugins/Casino.cs
@@ -7,10 +7,9 @@ using RustExtended;
 
 namespace Oxide.Plugins
 {
-    [Info("Casino", "kustanovich", 1.0)]
+    [Info("Casino", "kustanovich", 1.0.2)]
     class Casino : RustLegacyPlugin	 
 	{
-			int counter  = 0;
 			string chatName = "КАЗИНО";
 			private System.Random getrandom;
 
@@ -23,33 +22,66 @@ namespace Oxide.Plugins
 		[ChatCommand("casino")]
 		void casino(NetUser netuser, string command, string[] args)		
 		{
-                    rust.SendChatMessage(netuser, chatName, ("добро пожаловать в казино"));
-					rust.SendChatMessage(netuser, chatName, ("используй /casall"));
+                    rust.SendChatMessage(netuser, chatName, ("Добро пожаловать в казино, тут ты можешь все проиграть, а можешь и выиграть."));
+					rust.SendChatMessage(netuser, chatName, ("Полагайсяна свою удачу. Для входа в казино, используй команду [COLOR#388FFF]/casall"));
+					rust.SendChatMessage(netuser, chatName, ("Минимальный баланс для входа : [COLOR#388FFF]5000"));
 	
 		}
 		[ChatCommand("casall")]
 		void casall(NetUser netuser, string command, string[] args)
 		{
 			
-		
 		ulong balance = Economy.GetBalance(netuser.userID);
 		
 		if(balance >= 5000) {
-			counter++;
-				int rnd=getrandom.Next(1, 4);
+				int rnd=getrandom.Next(1, 9);
 				if(rnd == 1) 
 				{ 
-				Economy.BalanceSub(netuser.userID, 1000);
-				rust.SendChatMessage(netuser, chatName, (counter + " Нам очень жаль! Вы все проиграли ( [COLOR#388FFF]x0 [COLOR#FFFAFA]). Ваш баланс : [COLOR#388FFF]" + Economy.GetBalance(netuser.userID)));
+				Economy.BalanceSub(netuser.userID, balance);
+				rust.SendChatMessage(netuser, chatName, ("Нам очень жаль! Вы все проиграли ( [COLOR#388FFF]x0 [COLOR#FFFAFA]). Ваш баланс : [COLOR#388FFF]" + Economy.GetBalance(netuser.userID)));
 				} 
 				else if(rnd == 2) {
 				Economy.BalanceSub(netuser.userID, 1000);
-				rust.SendChatMessage(netuser, chatName, (counter + " Нам очень жаль! Вы проиграли ( [COLOR#388FFF]1000 [COLOR#FFFAFA]). Вам баланс : [COLOR#388FFF]" + Economy.GetBalance(netuser.userID)));
+				rust.SendChatMessage(netuser, chatName, ("Нам очень жаль! Вы проиграли ( [COLOR#388FFF]1000 [COLOR#FFFAFA]). Вам баланс : [COLOR#388FFF]" + Economy.GetBalance(netuser.userID)));
 								  }
-				else if(rnd == 3) {Economy.BalanceAdd(netuser.userID, 5000); rust.SendChatMessage(netuser, chatName, (counter + " Поздравляем вы выиграли ( [COLOR#388FFF]5000 [COLOR#FFFAFA]). Ваш баланс : [COLOR#388FFF]" + Economy.GetBalance(netuser.userID)));}
+				else if(rnd == 3) {
+					Economy.BalanceAdd(netuser.userID, 1000); 
+					rust.SendChatMessage(netuser, chatName, ("Поздравляем вы выиграли ( [COLOR#388FFF]1000 [COLOR#FFFAFA]). Ваш баланс : [COLOR#388FFF]" + Economy.GetBalance(netuser.userID)));
+					}
+				else if(rnd == 4) {
+					Economy.BalanceAdd(netuser.userID, 5000);
+					rust.SendChatMessage(netuser, chatName, ("Поздравляем вы выиграли ( [COLOR#388FFF]5000 [COLOR#FFFAFA]). Ваш баланс : [COLOR#388FFF]" + Economy.GetBalance(netuser, userID)));
+								}
+								else if (rnd == 5) {
+									Economy.BalanceSub(netuser.userID, 5000) 
+										rust.SendChatMessage(netuser, ChatName, ("Нам очень жаль! Вы проиграли ( [COLOR#388FFF] 5000 [COLOR#FFFAFA]). Ваш баланс : " + Economy.GetBalance(netuser, userID)));
+									}
+									else if (rnd == 6) {
+									Economy.BalanceSub(netuser.userID, 10000) 
+										rust.SendChatMessage(netuser, ChatName, ("Нам очень жаль! Вы проиграли ( [COLOR#388FFF] 10000 [COLOR#FFFAFA]). Ваш баланс : " + Economy.GetBalance(netuser, userID)));
+									
+									}
+									else if (rnd == 7) {
+										
+									Economy.BalanceAdd(netuser.userID, 10000) 
+										rust.SendChatMessage(netuser, ChatName, ("Поздравляем вы выиграли ( [COLOR#388FFF] 5000 [COLOR#FFFAFA]). Ваш баланс : " + Economy.GetBalance(netuser, userID)));
+									}
+									else if (rnd == 8) 
+										{
+									Economy.BalanceAdd(netuser.userID, 50000) 
+										rust.SendChatMessage(netuser, ChatName, ("Поздравляем вы выиграли ( [COLOR#388FFF] 50000 [COLOR#FFFAFA]). Ваш баланс : " + Economy.GetBalance(netuser, userID)));
+									}
+									else if (rnd == 9) 
+										{ 
+				Economy.BalanceSub(netuser.userID, balance);
+				rust.SendChatMessage(netuser, chatName, ("Нам очень жаль! Вы все проиграли ( [COLOR#388FFF]x0 [COLOR#FFFAFA]). Ваш баланс : [COLOR#388FFF]" + Economy.GetBalance(netuser.userID)));
+				} 
+										
 		}
+									
+									
 		else {rust.SendChatMessage(netuser, chatName, ("Баланс меньше 5000, ты не можешь играть в казинo!"));}
-		}
+		
 		
 
 	
